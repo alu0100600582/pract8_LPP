82ccde9bad49cd4737e9f9e796b74304b2d83a6b
Krishna Hemnani <alu0100536874@ull.edu.es>
2014-10-22 19:17:18 +0100

    Corrigiendo cambios

--- spec/spec_preguntas.rb
+++ spec/spec_preguntas.rb
@@ -7,17 +7,17 @@
     end
 
     it "Debe existir una pregunta" do
-      expect(@p.title).not_to be_empty
+      expect(@q.title).not_to be_empty
     end
    
     it "Deben existir opciones de respuestas" do
-      expect(@p.answers).not_to be_empty
+      expect(@q.answers).not_to be_empty
     end
 
      it "Se debe invocar a un metodo para obtener la pregunta" do
-       expect(@p.respond_to? :obtener_respuestas)
+       expect(@q.respond_to? :obtener_respuestas)
     end
    
     it "Se debe invocar a un metodo para obtener las opciones de respuesta" do
-       expect(@p1.respond_to? :obtener_respuestas)
+       expect(@q.respond_to? :obtener_respuestas)
     end
