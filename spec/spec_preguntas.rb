b0b162abaf823cc12799b33e0c5690eec96cd538
Krishna Hemnani <alu0100536874@ull.edu.es>
2014-10-22 19:12:23 +0100

    Añadiendo respuestas

--- spec/spec_preguntas.rb
+++ spec/spec_preguntas.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/Examenes'
+ require_relative '../lib/Examenes'
 
   describe Question do
  
@@ -6,4 +6,12 @@ require_relative '../lib/Examenes'
       @q = Question.new(" Cual es el resultado de sumar 2 y 5	?", ["4","5","7"], 0)  
     end
 
- 
+    it "Debe existir una pregunta" do
+      expect(@p.title).not_to be_empty
+    end
+   
+    it "Deben existir opciones de respuestas" do
+      expect(@p.answers).not_to be_empty
+    end
+    
+    
