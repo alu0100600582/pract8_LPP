ac3dcd70a817dfad9f06a329522901205b6f88e4
Krishna Hemnani <alu0100536874@ull.edu.es>
2014-10-28 12:22:00 +0000

    Añadiendo prueba de string(saludos) en la lista

--- spec/spec_listas.rb
+++ spec/spec_listas.rb
@@ -7,5 +7,16 @@
    it "Debe existir un Nodo de la lista con sus datos y su siguiente" do
     expect(Lista).to be		
   end
+  
+  
+  it " Se extrae el primer elemento de la lista" do
+    lista = Lista.new
+    lista.add("Bienvenidos")
+    lista.add("Buenas")
+    expect(lista.count).to eq(1)
+    expect(lista.remove).to eq('Bienvenidos')
+    expect(lista.count).to eq(1)
+  end
+
    
 end
