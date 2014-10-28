e5b9d996840c3270e0bf8c1c6edbe554e6d51eba
Francisco Mendoza <alu0100600582@ull.edu.es>
2014-10-28 11:59:54 +0000

    Inicializacion de la clase Node y de la clase Lista para la Lista Enlazada

--- lib/Listas.rb
+++ lib/Listas.rb
@@ -1,9 +1,35 @@
-  #require "Examenes/version"
+#require "Examenes/version"
+#!/usr/bin/env ruby
 
- #!/usr/bin/env ruby
-  #Clase 'Lista' genera una lista
-  class Lista
-    
-    
-    
+
+#Clase Node que implementa los nodos de la lista
+class Node
+
+  attr_accessor :link, :data
+
+  def initialize(content)
+    @link = nil
+    @data = content
   end
+
+end
+
+#Clase Lista que genera una Lista Enlazada
+class Lista
+
+  attr_reader :head
+
+#Metodo count que devuelve el numero de nodos de la lista 
+  def count
+    return 0 if head.nil?
+
+    node_count = 1
+    current = head
+    while current.link
+      current = current.link
+      node_count += 1
+    end
+    node_count
+  end
+
+end
