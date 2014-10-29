67670f5994514fea8a6cace39dfa726d99e8a671
Francisco Mendoza <alu0100600582@ull.edu.es>
2014-10-29 10:04:42 +0000

    Añadiendo las clases Node y Lista implementadas

--- lib/Listas.rb
+++ lib/Listas.rb
@@ -1,86 +1,63 @@
-#require "Examenes/version"
-#!/usr/bin/env ruby
-
-
-#Clase Node que implementa los nodos de la lista
 class Node
-    attr_accessor :value, :next
-
-    def initialize(value = nil)
-        @value = value
-    end
-
-    def to_s
-        @value
-    end
+	attr_reader :val, :siguiente
+	attr_writer :val, :siguiente
+	def initializer (val,siguiente)
+		@val = val
+		@siguiente = siguiente
+		@prev = nil
+	end
 end
 
-
-#Clase Lista que genera una Lista Enlazada
 class Lista
-
-    attr_accessor :head
-
-#Metodo count que cuenta los nodos de la lista
-   def count
-    return 0 if head.nil?
-
-    node_count = 1
-    current_node = head
-    while current_node.next
-      current_node = current_node.next
-      node_count += 1
-    end
-    node_count
-  end
-
-#Metodo initialize que inicia la lista enlazada
-    def initialize(first_value=nil)
-      @head = Node.new(first_value) if first_value
-    end
-#Metodo add que añade nodos a la lista
-    def add(value)
-      if head.nil?
-        head = Node.new(value)
-      else
-        current_node = @head
-        while current_node.next
-          current_node = current_node.next
-        end
-        current_node.next = Node.new(value)
-      end
-    end
-
-
-#Metodo find devuelve los nodos de la lista
-    def find(value)
-      current_node = head
-      while current_node != nil
-        return current_node if current_node.value == value
-        current_node = current_node.next
-      end
-      nil
-    end
-
-#Metodo remove que elimina nodos de la lista
-    def remove(value)
-      if head.value == value
-        head = head.next
-      else
-        current_node = head.next
-        prev_node = head
-        while current_node
-          if current_node.value == value
-            prev_node.next = current_node.next
-            return true
-          end
-          prev_node = current_node
-          current_node = current_node.next
-        end
-        nil
-      end
-    end
-
-
+	attr_reader :head, :tail
+	attr_writer :head, :tail
+	def initializer (vector)
+		@head = Node.new(vector[0],nil)
+		aux = Node.new(vector[1],nil)
+		@tail = aux
+		@head.siguiente = @aux
+		(2..vector.length).each do |i|
+			aux.siguiente = Node.new(vector[i],nil)
+			@tail = aux.siguiente
+		end
+	end
+	def pop_fin
+		aux = @tail.val
+		@tail.val = 0
+		@tail = @tail.prev
+		@tail.siguiente = nil
+		return aux
+	end
+	def pop_ini
+		aux = @head.val
+		@head = @head.siguiente
+		@head.prev = nil
+		return aux
+	end
+	def push_fin (val)
+		aux = Node.new(val,nil)
+		@tail = aux
+		@tail.prev.siguiente = @tail
+		return true
+	end
+	def push_ini (val)
+		aux = Node.new(val,@head)
+		@head = aux
+		return true
+	end
+	def push_fin_m (vector)
+		(0..vector.length).each do |i|
+			aux = Node.new(vector[i],nil)
+			@tail = aux
+			@tail.prev.siguiente = @tail	
+		end			
+		return true
+	end
+	def push_ini_m (vector)
+		(0..vector.length).each do |i|
+			aux = Node.new(val,@head)
+			@head = aux
+		end
+		return true
+	end
 end
-
