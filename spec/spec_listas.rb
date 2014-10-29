665bef1e3befbf8d4473abfcf34b80a5e6a4041f
Francisco Mendoza <alu0100600582@ull.edu.es>
2014-10-29 11:03:51 +0000

    Añadiendo la segunda parte de la practica (preguntas tipo test)

--- spec/spec_listas.rb
+++ spec/spec_listas.rb
@@ -2,51 +2,48 @@
  require_relative '../lib/Listas.rb'
 
  
- describe Lista do
-	 before :each do
-		@nodo1 = Node.new(3,@nodo2)
-		@nodo2 = Node.new(5,nil)
+ describe Node do 
+	
+        it "Debe existir un Nodo de la lista con sus datos y su siguiente" do
+		@n1 = Node.new(3,@n2)
+		@n2 = Node.new(5,nil)
 		@lista = Lista.new([3,5])
+		@n1.next = @n2
+		expect(@n1.val) == 3
+		expect(@n1.siguiente) == @n2 
 	end
-	describe "Valor de nodo1 = 3" do
-		it "Valor de nodo1 correcto" do
-			@nodo1.val.should eq(3)
-		end
-	end
-	describe "Nodo siguiente a nodo1 = nodo2" do
-		it "Nodo siguiente correcto" do
-			@nodo1.siguiente.should eq(@nodo2)
-		end
-	end
-	describe "Extracción de ultimo elemento de la lista" do
-		it "Extracción correcta" do
+end
+ 
+ 
+ describe Lista do
+
+		it "Extracción de ultimo elemento de la lista" do
 			@lista.pop_fin.should eq(5)
 		end
-	end
-	describe "Extracción del primer elemento de la lista" do
-		it "Extracción correcta" do
+	
+		it "Extracción del primer elemento de la lista" do
 			@lista.pop_ini.should eq(3)
 		end
-	end
-	describe "Inserción de elemento por el inicio" do
-		it "Elemento insertado correctamente" do
+	
+	
+		it "Inserción de elemento por el inicio" do
 			@lista.push_ini(8).should eq(true)
 		end
-	end
-	describe "Inserción de elemento por el final" do
-		it "Elemento insertado correctamente" do
+	
+
+		it "Inserción de elemento por el final" do
 			@lista.push_fin(6).should eq(true)
 		end
-	end
-	describe "Inserción de varios elementos" do
-		it "Inserción multiple correcta" do
+	
+
+		it "Inserción de varios elementos" do
 			@lista.push_m([9,7,4]).should eq(true)
 		end
-	end
-	describe "Consulta de cabeza" do
-		it "Consulta de cabeza correcta" do
+	
+
+		it "Consulta de cabeza " do
 			@lista.head.should eq(3)
   		end 
-end
  end
+
  
