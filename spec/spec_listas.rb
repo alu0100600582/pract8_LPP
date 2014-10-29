9277ca33ea5bdf5f212da9b2fd4dcb71192b2eae
Francisco Mendoza <alu0100600582@ull.edu.es>
2014-10-29 09:24:59 +0000

    Añadiendo las nuevas espectativas

--- spec/spec_listas.rb
+++ spec/spec_listas.rb
@@ -1,22 +1,51 @@
  require 'spec_helper'
- require_relative '../lib/Listas.rb'
+ require '../lib/Listas.rb'
 
  
  describe Lista do
-
-   it "Debe existir un Nodo de la lista con sus datos y su siguiente" do
-    expect(Lista).to be		
-  end
-  
-  
-  it " Se extrae el primer elemento de la lista" do
-    lista = Lista.new
-    lista.add(5)
-    lista.add(6)
-    expect(lista.count).to eq(2)
-    expect(lista.remove).to eq(5)
-    expect(lista.count).to eq(1)
-  end
+	 before :each do
+		@nodo1 = Struct.new(3,@nodo2)
+		@nodo2 = Struct.new(5,nil)
+		@lista = ListaEnlazada.new([3,5,6,7])
+	end
+	describe "Valor de nodo1 = 3" do
+		it "Valor de nodo1 correcto" do
+			@nodo1.val.should eq(3)
+		end
+	end
+	describe "Nodo siguiente a nodo1 = nodo2" do
+		it "Nodo siguiente correcto" do
+			@nodo1.siguiente.should eq(@nodo2)
+		end
+	end
+	describe "Extracción de ultimo elemento de la lista" do
+		it "Extracción correcta" do
+			@lista.pop_fin.should eq(7)
+		end
+	end
+	describe "Extracción del primer elemento de la lista" do
+		it "Extracción correcta" do
+			@lista.pop_ini.should eq(3)
+		end
+	end
+	describe "Inserción de elemento por el inicio" do
+		it "Elemento insertado correctamente" do
+			@lista.push_ini(8).should eq(true)
+		end
+	end
+	describe "Inserción de elemento por el final" do
+		it "Elemento insertado correctamente" do
+			@lista.push_fin(6).should eq(true)
+		end
+	end
+	describe "Inserción de varios elementos" do
+		it "Inserción multiple correcta" do
+			@lista.push_m([9,7,4]).should eq(true)
+		end
+	end
+	describe "Consulta de cabeza" do
+		it "Consulta de cabeza correcta" do
+			@lista.head.should eq(3)
 
    
 end
