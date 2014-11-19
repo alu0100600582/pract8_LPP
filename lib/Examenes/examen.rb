89193547ec383bbc10fb87ec885c05714dc3c337
Francisco Mendoza <alu0100600582@ull.edu.es>
2014-11-19 18:04:06 +0000

    Modificando el codigo de la practica

---
+++ lib/Examenes/examen.rb
@@ -0,0 +1,15 @@
+
+
+class Examen
+
+ attr_accessor :exam, :npreguntas
+    def initialize(n)
+      @exam= List.new
+      @npreguntas=n
+    end
+   
+    def add_pregunta(pre)
+        @exam.push(pre)
+    end
+   
+end  
