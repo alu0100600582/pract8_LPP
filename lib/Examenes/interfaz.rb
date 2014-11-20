ef3b526d1c87a90cd806c91cf84031b126e8f9f4
Francisco Mendoza González <alu0100600582@ull.edu.es>
2014-11-20 12:50:27 +0000

    Codigo en su etapa final

--- lib/Examenes/interfaz.rb
+++ lib/Examenes/interfaz.rb
@@ -11,25 +11,25 @@ class Interface
     def make_exam
         i = 0
         puts @theExam.printQuestion
-            while @userAnswers.size() > i
-            puts @theExam.printQuestion
-            print "--> Respuesta = "
-            @userAnswers[i] = gets.chomp
-            if @userAnswers[i] == @theExam.examAnswers[i]
-            @numRight += 1
-            end
-            i += 1
-            end
+        while @userAnswers.size() > i
+        puts @theExam.printQuestion
+        print "--> Respuesta = "
+        @userAnswers[i] = gets.chomp
+        if @userAnswers[i] == @theExam.examAnswers[i]
+        @numRight += 1
+        end
+        i += 1
+        end
         puts " \n---- Resultado del examen ------ "
-            if pass
-            puts " ## --> Aprobado."
-            else
-            puts " ## --> Suspendido."
-            end
+        if pass
+        puts " ## --> Aprobado."
+        else
+        puts " ## --> Suspendido."
+        end
     end
     
-private
+    private
     def pass
-    @userAnswers.size()/2 < @numRight
+        @userAnswers.size()/2 < @numRight
     end
 end
