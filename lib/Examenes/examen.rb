cba9f06c6f311ec4d06038974a80633648bbd414
Francisco Mendoza González <alu0100600582@ull.edu.es>
2014-11-25 10:56:15 +0000

    commit necesario

--- lib/Examenes/examen.rb
+++ lib/Examenes/examen.rb
@@ -9,5 +9,27 @@ class Examen
     def add_pregunta(pre)
         @exam.push(pre)
     end
-   
+    
+    def resolver(resp)
+				preg_actual=0
+				preg_correctas=0
+					while preg_actual<@npreguntas
+						preg_actual=preg_actual+1
+						puts @exam.get_ini().pregunta
+						puts @exam.get_ini().respuesta
+						puts ""
+						
+						if @exam.get_ini().resp_correcta(resp[preg_actual-1])
+							preg_correctas=preg_correctas+1
+						end
+						@exam.pop()
+						puts""
+					end
+					print "Ha respondido bien "
+					print preg_correctas
+					print " preguntas de "
+					print @npreguntas
+					return preg_correctas
+    end
+    
 end  
