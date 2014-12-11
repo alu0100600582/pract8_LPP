b0971c9f2bf7d41f46b6a1d2a75d832564bf4b96
Francisco Mendoza González <alu0100600582@ull.edu.es>
2014-12-11 03:14:47 +0000

    Añadiendo la clase Quiz

--- Rakefile
+++ Rakefile
@@ -3,9 +3,8 @@ require "bundler/gem_tasks"
 task :default => :spec
 desc "Ejecutar las espectativas de la clase simpleselect"
 task :spec do
-#sh "rspec -I. spec/spec_preguntasimple.rb"
-#sh "rspec -I. spec/spec_listasNodo.rb"
- sh "rspec -I. spec/spec_examen.rb" 
+#sh "rspec -I. spec/spec_examen.rb"
+sh "rspec -I. spec/quiz_spec.rb"
 end
 
 
--- lib/Examenes/quiz.rb
+++ lib/Examenes/quiz.rb
@@ -1,72 +1,77 @@
-# -*- coding: utf-8 -*-
 
-require 'rubygems'
-require 'bundler/setup'
-require 'rspec'
+class Quiz
+    attr_accessor :name, :questions, :answers
 
+    def initialize name, &block
+                @name = name
+                @questions = []
+                @answers = []
+                @i = 0
+                @counter = 0
 
-module Examenes
-    
-    class Quiz
-        attr_accessor :exam, :name
-            
-        def initialize(name = "Sin Nombre", &block)
-            @exam = ExamenCompleto.new(List.new)
-            @name = name
-            
-            instance_eval &block
-        end
-        
-        def question(text, options = {})
-            distractors = []
-            distractors << options[:wrong] while options[:wrong]
-            
-            if(options[:right] == 'FALSE' || options[:right] == 'TRUE')
-                add(:type => 0, :text => text, :right => options[:right])
-            elsif
-                add(:type => 1, :text => text, :right => options[:right] , :distractor => distractors )
+        if block_given?
+            if block.arity == 1
+                yield self
+            else
+                instance_eval &block
             end
-        end
-        
-        def add(args) #Añade una pregunta al examen
-            if args[:type] == 1 #La pregunta es SimpleChoice
-                q = PreguntaSimple.new(
-                    :text => args[:text],
-                    :right => args[:right],
-                    :distractor => args[:distractor])
-            elsif args[:type] == 0 #La pregunta es TrueFalse
-                q = VerdaderoFalso.new(
-                    :text => args[:text],
-                    :right => args[:right])
-            end
-            
-            (@exam.list).push(:elm => [q])
-        end
-        
-        def check(resp)
-            resul = []
-            i=0
-            (@exam.order).each do |quest|
-                resul << (resp[i] == quest.right)
-                i += 1
-            end
-            resul
-        end
-        
-        def show
-            @name + @exam.show
-        end
-
-        def show_i
-            @name + @exam.show_i
-        end
-
-        def right
-            :right
-        end
-
-        def wrong
-            :wrong
         end
     end
-end    
+
+    def to_s
+        output = name
+        output << "\n#{'=' * name.size}\n\n"
+        questions.each_with_index do |question, index|
+            output << "#{index+1}) #{question}\n"
+        end
+        output
+    end	
+
+    def question(name, options = {})
+        @counter = 0
+        q = name
+        answers << {}
+        answers[@i][:wrong] = []
+
+        if options[:right]
+            answers[@i][:right] = options[:right]
+            q << "\n\t#{@counter+=1}) #{options[:right]}"
+        end
+
+        if options[:wrong]
+            answers[@i][:wrong] << options[:wrong]
+            q << "\n\t#{@counter+=1}) #{options[:wrong]}"
+        end
+        
+        questions << q
+        @i += 1
+    end
+
+    def run eleccion
+        cor = ""
+        
+        cor << "#{name}\n#{'=' * name.size}\n\n"
+        questions.each_with_index do |question, index|
+            cor << "#{index+1}) #{question}\n"
+            cor << "Respuesta: #{eleccion[index]}\n"
+        end
+        contador = 0
+
+        eleccion.each_with_index do |x,i|
+            if (x == answers[i][:right])
+                cor << "Pregunta #{i+1} correcta.\n"
+                contador+=1
+            else
+                cor << "Pregunta #{i+1} incorrecta.\n"
+            end
+        end
+            if contador == eleccion.size
+                cor << "Nota: Sobresaliente.\n"
+            elsif contador >= eleccion.size/2
+                cor << "Nota: Aprobado.\n"
+            else
+                cor << "Nota: Suspendido.\n"
+            end
+            cor
+    end
+end
