d97d3092117d521702f6e83832f4d7db977ba918
Francisco Mendoza González <alu0100600582@ull.edu.es>
2014-12-18 11:05:02 +0000

    Añadiendo codigo funcionando

--- lib/Examenes/naranjero.rb
+++ lib/Examenes/naranjero.rb
@@ -1,57 +1,73 @@
-class Arbol
+# -*- coding: utf-8 -*-
+
+class Arbol_N
     require 'thread'
-    attr_reader :edad, :altura, :contador
-    
-    def initialize()
+
+    def initialize
         @edad = 0
         @altura = 0
-        @contador = 0
-        @vivo=true
+        @count_naranjas = 0
+        @se_puede_recolectar = true
+        @arbol_muerto = false
     end
-    
-     def recolectar_una(n)
-        m=0
-        mutex1=Mutex.new
-        while (n>m)
-    
-       if @vivo == true
-           if @naranjas >0
-               mutex1.lock
-               @naranjas= @naranjas-1
-               mutex1.unlock
-               puts "¡¡La naranja estaba deliciosa!! quedan #{@naranjas} naranjas"
-           else
-               puts "El árbol no tiene naranjas"
-           end
-        else
-            puts "El árbol está muerto"
-        end
-        m=m+1
-        sleep(2)
-        end
-    end
-    
-    def uno_mas
-		if @vivo 
-			@edad = @edad + 1
-			@altura = @altura + 0.4
-			
-			if @altura > 10 && rand(2) > 0 	#Si se cumple esta condicion el arbol muere
-				@estaVivo = false
-				"El arbol ha muerto\n"
-			elsif @altura > 2 	#Si se cumple esta condicion el arbol dara frutos
-				@contador = (@altura * 15 - 25).to_i
-				"El árbol mide #{@altura.round(2)}"
-			else
-				"El árbol mide #{@altura.round(2)}"+
-				" y es demasiado jover para dar frutos \n"
-			end
-		else
-				"El arbol sigue muerto\n"
-		end
-	end
-    
-end
 
-if __FILE__ == $0 then 
+    def recolectar_una
+        if @count_naranjas > 0
+            @se_puede_recolectar = true
+        else
+            @se_puede_recolectar = false
+        end
+
+        if @se_puede_recolectar == true
+            @count_naranjas = @count_naranjas - 1
+            puts "Deliciosa, te quedan #{@count_naranjas} todavia.\n"
+        else
+            puts "Ups!!Lo siento no hay naranjas\n"
+        end
+    end
+
+    def uno_mas
+        if @arbol_muerto == false
+            @edad = @edad + 1 #la edad se va aumentando de uno en uno
+            puts "AUMENTANDO AÑOS, Ahora tiene #{@edad} años\n"
+            @altura = @altura + 2 #la altura se va aumentando en 2
+            puts "CRECIENDO, Ahora tiene #{@altura} metros\n"
+            if @edad < 2
+                @count_naranjas = @count_naranjas + 5
+            end
+            if @edad >= 2 #si el arbol tiene 2 años o mas produce mas
+            @count_naranjas = @count_naranjas + 10
+            end
+            if @edad > 7 #si el arbol llega a los 10 años muere
+                puts "**************************************"
+                puts "El Arbol ha MUERTO!!\n"
+                puts "**************************************"
+                @arbol_muerto = true
+                @count_naranjas = 0
+            end
+        end
+    end
+
+    def contador
+        puts @count_naranjas
+        return @count_naranjas
+    end
+
+    def edad
+        return @edad
+    end
+
+    def altura
+        return @altura
+    end
+
+    def arbol_muerto
+        if @arbol_muerto == true
+            return true
+        end
+    end
+end 
+
+
+if __FILE__ == $0 then #si se utiliza desde un require estas lineas no se ejecutan pero si lo ejecutamos en consola funciona para ejecutar
 end
--- spec/spec_naranjero.rb
+++ spec/spec_naranjero.rb
@@ -2,31 +2,86 @@ require 'Examenes'
 require 'spec_helper'
 
 
-describe Arbol do
+describe Arbol_N do
     before :each do
-        @naranjero = Arbol.new(0,0)
-        @t1 = Thread.new do
-            20.times do
-                @naranjero.uno_mas
-            end
-        end
-        @t2 = Thread.new do
-            20.times do
-                @naranjero.recolectar_una
-            end
-        end
-    end
-    
-    describe "Aumentar_edad" do
-        it "edad" do
-            @naranjero.uno_mas
-            expect(@naranjero.edad).to eq(1)     
-        end
-        it "vivo?" do
-            @t1.join
-            sleep 0.1
-            expect(@naranjero.vivo).to eq(false)
-        end
+        @arbol_naranjero = Arbol_N.new
     end
 
+context "Pruebas para la clase Arbol_N y los hilos de ejecucion" do
+    it "El atributo es de la clase" do
+        expect(@arbol_naranjero.class).to eq(Arbol_N)
+    end
+
+    it "Existe un metodo para recolectar una naranja" do
+        expect(@arbol_naranjero).to respond_to :recolectar_una
+    end
+
+    it "Existe un metodo para aumentar edad y altura y comprobrar si el arbol esta muerto" do
+        expect(@arbol_naranjero).to respond_to :uno_mas
+    end
+  
+    it "Existe un metodo que devuelva el contador de naranjas" do
+        expect(@arbol_naranjero).to respond_to :contador
+    end
+  
+    it "Existe un metodo que devuelva si el arbol esta muerto" do
+        expect(@arbol_naranjero).to respond_to :arbol_muerto
+    end
+  
+    it "Existe un metodo que devuelva la edad del arbol" do
+        expect(@arbol_naranjero).to respond_to :edad
+    end
+###################CREACION DE HILOS CON SUS PRUEBAS#####################
+    it "Pruebas para probar la ejecucion con los hilos" do
+        @hilo_uno = Thread.new do
+            @arbol_naranjero.uno_mas
+            sleep(2)
+            @arbol_naranjero.uno_mas
+            sleep(2)
+            @arbol_naranjero.recolectar_una
+            sleep(0.1)
+            @arbol_naranjero.recolectar_una
+            sleep(0.1)
+            @arbol_naranjero.recolectar_una
+            sleep(0.1)
+            @arbol_naranjero.recolectar_una
+            sleep(0.1)
+            @arbol_naranjero.recolectar_una
+            sleep(0.1)
+            @arbol_naranjero.uno_mas
+            sleep(1)
+            @arbol_naranjero.uno_mas
+            sleep(1)
+            @arbol_naranjero.recolectar_una
+            sleep(1)
+            @arbol_naranjero.uno_mas
+            sleep(1)
+            @arbol_naranjero.recolectar_una
+            sleep(1)
+            @arbol_naranjero.uno_mas
+            sleep(1)
+        end #fin de hilo_uno
+
+        @hilo_dos = Thread.new do
+            @arbol_naranjero.recolectar_una
+            sleep(0)
+            @arbol_naranjero.uno_mas
+            sleep(2)
+            @arbol_naranjero.uno_mas
+            sleep(2)
+            @arbol_naranjero.uno_mas
+        end#fin de hilo_dos
+            
+        @hilo_uno.join
+        @hilo_dos.join
+        
+    expect(@hilo_uno.class).to eq(Thread)
+    expect(@hilo_dos.class).to eq(Thread)
+    expect(@arbol_naranjero.edad).to eq(8)
+    expect(@arbol_naranjero.contador).to eq(0)
+    expect(@arbol_naranjero.altura).to eq(16)
+    expect(@arbol_naranjero.arbol_muerto).to eq(true)
+    end
 end
+end
+
